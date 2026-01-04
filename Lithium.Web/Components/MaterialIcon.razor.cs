594cb01cec8e5f1e13f131f4e10d3ae281cb63c9
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-04 16:23:35 +0100

    Use Lucide icons instead of Material icons. Update icons

--- Lithium.Web/Components/MaterialIcon.razor.cs
+++ Lithium.Web/Components/MaterialIcon.razor.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace Lithium.Web.Components;
 
-public partial class Icon : ComponentBase
+public partial class MaterialIcon : ComponentBase
 {
     [Parameter] public RenderFragment? ChildContent { get; set; }
     [Parameter] public bool Filled { get; set; }
