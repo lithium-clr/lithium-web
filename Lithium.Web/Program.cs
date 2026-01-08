df7916c3ee56b18e83ccca0be625321b27b810af
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-08 21:43:45 +0100

    Add localization for landing page. Localization is based on user country.

--- Lithium.Web/Program.cs
+++ Lithium.Web/Program.cs
@@ -23,6 +23,8 @@ var client = new MongoClient(connectionString);
 builder.Services.AddMongoDB<WebDbContext>(client, "web");
 builder.Services.AddScoped<UserCollection>();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 // Data Protection - IMPORTANT pour Cloudflare
 var dataProtectionPath = Path.Combine("/home/app/.aspnet/DataProtection-Keys");
 builder.Services.AddDataProtection()
@@ -48,6 +50,8 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
     options.KnownProxies.Clear();
 });
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // IMPORTANT : Forwarded headers AVANT tout le reste
@@ -65,10 +69,23 @@ else
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 
+// app.UseStaticFiles();
 app.UseAntiforgery();
 
+var supportedCultures = new[] { "en-US", "fr-FR" };
+
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllers();
 
 app.Run();
