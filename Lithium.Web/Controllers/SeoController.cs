0a27b9d425eb56260e3a93d645aa5819862a6fad
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-09 14:40:15 +0100

    Add robots.txt and basic auto generated sitemap.xml

---
+++ Lithium.Web/Controllers/SeoController.cs
@@ -0,0 +1,97 @@
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lithium.Web.Controllers;
+
+public sealed class SeoController(IWebHostEnvironment env) : Controller
+{
+    [Route("/robots.txt")]
+    public ContentResult RobotsTxt()
+    {
+        var sb = new StringBuilder().AppendLine("User-agent: *");
+
+        sb.AppendLine(env.IsProduction() ? "Allow: /" : "Disallow: /");
+        sb.AppendLine($"Sitemap: {Request.Scheme}://{Request.Host}/sitemap.xml");
+
+        return Content(sb.ToString(), "text/plain", Encoding.UTF8);
+    }
+
+    [Route("/sitemap.xml")]
+    public ContentResult Sitemap()
+    {
+        var xmlns = XNamespace.Get("http://www.sitemaps.org/schemas/sitemap/0.9");
+        var xhtml = XNamespace.Get("http://www.w3.org/1999/xhtml");
+
+        var root = new XElement(xmlns + "urlset", new XAttribute(XNamespace.Xmlns + "xhtml", xhtml));
+
+        var pages = typeof(SeoController).Assembly.ExportedTypes
+            .Where(p => p.IsSubclassOf(typeof(Microsoft.AspNetCore.Components.ComponentBase)) &&
+                        p.Namespace != null &&
+                        p.Namespace.StartsWith("Lithium.Web.Components.Pages"))
+            .ToList();
+
+        var webSiteUrl = $"{Request.Scheme}://{Request.Host}";
+
+        foreach (var page in pages)
+        {
+            var routes = page.GetCustomAttributes<Microsoft.AspNetCore.Components.RouteAttribute>().ToList();
+
+            foreach (var route in routes)
+            {
+                var pageUrl = route.Template;
+                if (pageUrl.Contains('{')) continue;
+
+                var url = new XElement(xmlns + "url",
+                    new XElement(xmlns + "loc", $"{webSiteUrl}{pageUrl}")
+                );
+
+                if (routes.Count > 1)
+                {
+                    foreach (var altRoute in routes)
+                    {
+                        var altUrl = altRoute.Template;
+                        if (altUrl.Contains('{')) continue;
+
+                        var segments = altUrl.Split('/', StringSplitOptions.RemoveEmptyEntries);
+                        var lang = "x-default";
+
+                        if (segments.Length > 0)
+                        {
+                            var segment = segments[0];
+                            if (segment.Length == 2 || (segment.Length == 5 && segment.Contains('-')))
+                            {
+                                lang = segment;
+                            }
+                        }
+
+                        if (altUrl == "/") lang = "x-default";
+
+                        url.Add(new XElement(xhtml + "link",
+                            new XAttribute("rel", "alternate"),
+                            new XAttribute("hreflang", lang),
+                            new XAttribute("href", $"{webSiteUrl}{altUrl}")));
+                    }
+                }
+
+                root.Add(url);
+            }
+        }
+
+        var xDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), root);
+        var sb = new StringBuilder();
+
+        using (var writer = new Utf8StringWriter(sb))
+        {
+            xDoc.Save(writer);
+        }
+
+        return Content(sb.ToString(), "text/xml", Encoding.UTF8);
+    }
+
+    private class Utf8StringWriter(StringBuilder sb) : StringWriter(sb)
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
